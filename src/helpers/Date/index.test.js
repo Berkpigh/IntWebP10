913e80bb328e34e23d851c799b6fb94edcb63fa0
Berkpigh <pierre.berkovits@netplus.ch>
2024-01-03 20:18:25 +0100

    Date

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -8,10 +8,12 @@ describe("Date helper", () => {
         const da1 = new Date('2022-01-01')
         it("the function return janvier for 2022-01-01 as date", () => {
             expect(getMonth(da1)).toBe('janvier')
+            setTimeout(() => { }, 2000);
         });
         it("the function return juillet for 2022-07-08 as date", () => {
             const da2 = new Date('2022-07-08')
             expect(getMonth(da2)).toBe('juillet')
+            setTimeout(() => { }, 1000);
         });
         it("the function not return juillet for 2022-08-08 as date", () => {
             const da3 = new Date('2022-08-08')
