08bdcebaf6899643195a92dfbcc5bd3700458e15
Berkpigh <pierre.berkovits@netplus.ch>
2024-01-07 17:41:13 +0100

    slider test

--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -8,7 +8,7 @@ const data = {
       title: "World economic forum",
       description:
         "Oeuvre à la coopération entre le secteur public et le privé.",
-      date: "2022-02-29T20:28:45.744Z",
+      date: "2022-02-28T20:28:45.744Z",
       cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
     {
@@ -25,9 +25,23 @@ const data = {
     },
   ],
 };
+describe("Quand le slider s'affiche", () => {
+  it("Scénario 3 :La carte de l'événement de plus récent s'affiche", async () => {
+    window.console.error = jest.fn();
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+    await screen.findByText("World Farming Day");
+    await screen.findByText("janvier");
+    await screen.findByText("Evenement mondial autour de la ferme");
+  });
+});
 
-describe("When slider is created", () => {
-  it("a list card is displayed", async () => {
+describe("Quand le slider s'affiche", () => {
+  it("Scénario 3 : puis celle de l'événement un peu moins récent", async () => {
     window.console.error = jest.fn();
     api.loadData = jest.fn().mockReturnValue(data);
     render(
@@ -36,9 +50,26 @@ describe("When slider is created", () => {
       </DataProvider>
     );
     await screen.findByText("World economic forum");
-    await screen.findByText("janvier");
+    await screen.findByText("février");
     await screen.findByText(
       "Oeuvre à la coopération entre le secteur public et le privé."
     );
-  });
-});
+  })
+})
+describe("Quand le slider s'affiche", () => {
+  it("Scenari 2 et 3 : puis le troisième événement", async () => {
+    window.console.error = jest.fn();
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+    await screen.findByText("World Gaming Day");
+    await screen.findByText("mars");
+    await screen.findByText(
+      "Evenement mondial autour du gaming"
+    );
+    })
+})
+
